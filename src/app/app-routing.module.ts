601b230414abb361d952b9410784e8a587951ece
Craig Terry <crgtrr@gmail.com>
2019-10-30 18:25:04 -0500

    initial commit - 4 components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  // {path: '/', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent },
+  {path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { TodoItemComponent } from './dashboard/todo-item/todo-item.component';
+import { NewTodoItemComponent } from './dashboard/new-todo-item/new-todo-item.component';
+import { TodoListComponent } from './dashboard/todo-list/todo-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TodoItemComponent,
+    NewTodoItemComponent,
+    TodoListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
